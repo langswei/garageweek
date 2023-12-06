462164f3fde548f8a1928d3c92a8d75b07cdfcdc
Damian Langsweirdt <langswei@adobe.com>
2023-12-06 11:48:21 -0600

    typo

--- blocks/ims/ims.js
+++ blocks/ims/ims.js
@@ -9,7 +9,7 @@ export default async function decorate(block) {
 
     window.adobeid = {
         client_id: 'garage-week-ims',
-        scope: 'AdobeID,openid,additonal_info.ownerOrg,read_organizations',
+        scope: 'AdobeID,openid,additional_info.ownerOrg,read_organizations',
         locale: 'en_US',
         environment: 'stg1',
         useLocalStorage: false,
